944a08544ef255da3ceaa995b5145846334d89d2
ankitdobhal <dobhal.ankit@protonmail.com>
2020-10-18 12:10:05 -0400

    Updated fakeit tool

--- fakeit.py
+++ fakeit.py
@@ -1,19 +1,39 @@
-#code for subdomain finder
-#proxies for subdomain finder
-#email the subdomains.
-
-pip3 install requests
+#!/usr/bin/python3
 import requests
+import argparse
+from colorama import Fore
 
-domain = "apple.com"
-file = open("subdomains.txt")
-content = file.read()
-subdomains = content.splitlines()
-for subdomain in subdomains:
-    url = f"http://{subdomain}.{domain}"
-    try:
-        requests.get(url)
-    except requests.ConnectionError:
-        pass
-    else:
-        print("[+] Discovered subdomain:", url)
+def subdomainer(domain):
+	file = open("subdomains.txt")
+	content = file.read()
+	subdomains = content.splitlines()
+	for subdomain in subdomains:
+		url = f"http://{subdomain}.{domain}"
+		try:
+			requests.get(url)
+		except requests.ConnectionError:
+			pass
+		else:
+			print(Fore.BLUE + "[+] Discovered subdomain:", url)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--domain',help='Target domain ')
+	args = parser.parse_args()
+	print(Fore.RED + '''
+
+  █████▒▄▄▄       ██ ▄█▀▓█████  ██▓▄▄▄█████▓
+▓██   ▒▒████▄     ██▄█▒ ▓█   ▀ ▓██▒▓  ██▒ ▓▒
+▒████ ░▒██  ▀█▄  ▓███▄░ ▒███   ▒██▒▒ ▓██░ ▒░
+░▓█▒  ░░██▄▄▄▄██ ▓██ █▄ ▒▓█  ▄ ░██░░ ▓██▓ ░ 
+░▒█░    ▓█   ▓██▒▒██▒ █▄░▒████▒░██░  ▒██▒ ░ 
+ ▒ ░    ▒▒   ▓▒█░▒ ▒▒ ▓▒░░ ▒░ ░░▓    ▒ ░░   
+ ░       ▒   ▒▒ ░░ ░▒ ▒░ ░ ░  ░ ▒ ░    ░    
+ ░ ░     ░   ▒   ░ ░░ ░    ░    ▒ ░  ░      > v1.0 Subdomain Finder
+             ░  ░░  ░      ░  ░ ░           
+
+''')
+
+	if args.domain:
+		subdomainer(args.domain)
+		exit()
